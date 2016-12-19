12dfb68e61a6be8edfccf3a4d1ff0af4f785376d
github username <github email address>
2016-12-19 03:00:42 -0500

    whew

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,48 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(name: params[:name], content: params[:content])
+    redirect '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(id = params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(id = params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(id = params[:id])
+    # binding.pry
+    @post.update(name: params[:name], content: params[:content])
+    redirect 'posts/'"#{@post.id}"
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(id = params[:id])
+    @post_name = @post.name
+    # Need to store the name before the instance gets deleted.
+    @post.delete
+    erb :deleted
+  end
+
+
 end
---
+++ app/models/post.rb
@@ -0,0 +1,26 @@
+class Post < ActiveRecord::Base
+
+  # attr_accessor :name, :content
+  #
+  # @@posts = []
+  #
+  # def initialize(name:, content:)
+  #   @name = name
+  #   @content = content
+  #   @@posts << self
+  #   binding.pry
+  # end
+  #
+  # def self.all
+  #   @@posts
+  # end
+
+  # def initialize(opts)
+  #   opts.each do |attribute, value|
+  #     self.send("#{attribute}=", value)
+  #   end
+  # end
+
+  # KEEP THIS EMPTY!!!
+
+end
